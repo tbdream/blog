18fa7ffef60d9512aa0babfb7d58c73dc9311df9
felixbo86 <tbdream@126.com>
2020-03-26 22:17:20 +0800

    init

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  title: 'felixbo’s blog',
+  description: 'This is a blog',
+  theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+
+  plugins: ['@vuepress/blog'],
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/ulivz',
+        },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/_ulivz',
+        },
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+  },
+}
